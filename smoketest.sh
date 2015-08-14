d111957b26306eb7db5c280cf532e3c340337ecd
Chris DeLashmutt <cdelashmutt@pivotal.io>
2015-08-14 00:12:26 -0400

    Update smoketest.sh

--- smoketest.sh
+++ smoketest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bosh target $1
-bosh login $2 $3
-bosh download manifest $4 $4.yml
-bosh deployment $4.yml
-bosh run errand smoke-tests
+bosh -n target $1
+bosh -n login $2 $3
+bosh -n download manifest $4 $4.yml
+bosh -n deployment $4.yml
+bosh -n run errand smoke-tests
